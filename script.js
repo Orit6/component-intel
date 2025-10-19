64e2dfe022ae06c17f621f37e3cde8d001bba9eb
Orit6 <79094168+Orit6@users.noreply.github.com>
2025-10-19 12:35:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ document.getElementById("searchBtn").addEventListener("click", async () => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": \`Bearer \${token}\`,
+        "Authorization": `Bearer ${token}`,
       },
       body: JSON.stringify(query),
     });
